7caa0d756b46f3c17bb5e3c5cbb8a56e67594800
matthieufelicite <matthieufelicite@icloud.com>
2025-06-27 11:35:32 +0200

    Add template

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import EmailList from "../components/EmailList.vue";
+import AddEmail from "../components/AddEmail.vue";
+</script>
+
+<template>
+<main class="p-6">
+    <div class="flex flex-col gap-6">
+        <div class="flex justify-end">
+            <AddEmail />
+        </div>
+        <EmailList />
+    </div>
+</main>
+</template>
